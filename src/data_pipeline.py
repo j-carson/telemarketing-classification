5d23b6cef53d5a52e0fb2cac9df66e3d806bdf4b
j-carson <44308120+j-carson@users.noreply.github.com>
2019-02-10 11:43:31 -0800

    add GridSearch option

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -2,14 +2,26 @@ import pandas as pd
 import numpy as np
 
 from imblearn.over_sampling import RandomOverSampler, SMOTENC
-from sklearn.preprocessing import StandardScaler
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.pipeline import make_pipeline as imb_make_pipeline
+from imblearn.pipeline import Pipeline as imbPipeline
 
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, PowerTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+
+import warnings
+from sklearn import exceptions
+warnings.filterwarnings("ignore", category=exceptions.DataConversionWarning)
 
 def sort_columns_by_type(columns):
     """
     sort_columns_by_type(columns):
     inputs: the columns in the model
-    returns two lists:  categorical_columns, numerical_columns
+    indices: returns column indices rather than column names (for smotenc)
+    returns three lists:  categorical_columns, numerical_columns, power_columns
     so they can be scaled/dummied separately
     """
 
@@ -25,12 +37,17 @@ def sort_columns_by_type(columns):
         'day_of_week',
         'poutcome'
     ]
-
-    numerical_cols = [
-        'age',
+    
+    # --- columns I'm sending to PowerTransformer
+    power_cols = [
         'campaign',
-        'pdays',
         'previous',
+        'age'
+    ]
+
+    # -- columns that go to StandardScaler
+    numerical_cols = [
+        'pdays',  # -- We still need a data cleaning strategy for this guy! 
         'emp_var_rate',
         'cons_price_idx',
         'cons_conf_idx',
@@ -38,6 +55,7 @@ def sort_columns_by_type(columns):
         'nr_employed'
     ]
     
+    # these show up in the sql query but are not predictors
     ignore_cols = [
         'success',
         'bank_addl_id'
@@ -45,106 +63,173 @@ def sort_columns_by_type(columns):
 
     selected_numerical = []
     selected_categorical = []
+    selected_power = []
     
-    for c in columns:
-        if c in numerical_cols:
-            selected_numerical += [c]
-        elif c in categorical_cols:
-            selected_categorical += [c]
-        elif c not in ignore_cols:
-            raise Exception('Unknown column', c)
+    for co in columns:
+        if co in numerical_cols:
+            selected_numerical += [co]
+        elif co in categorical_cols:
+            selected_categorical += [co]
+        elif co in power_cols:
+            selected_power += [co]
+        elif co not in ignore_cols: 
+            raise Exception('Unknown column', co)
+            
+    # -- returns are alphabetical    
+    return selected_categorical, selected_numerical, selected_power
+
+
+
+def read_data(columns):   
+    # -- Read the data -- # 
+    database = "postgresql://localhost/bankcalls"   
+    sql  = "SELECT " + ", ".join(columns) + " FROM bank_addl;"
+    
+    df =  pd.read_sql(sql, database, index_col='bank_addl_id')
+    
+    # -- Set success column to 0/1 and separate X's and y's -- #      
+    y = df['success'].replace({'yes':1, 'no':0})   
+    X = df.drop(columns='success')
+   
+    # --
+    # organize columns by type for ColumnTransformer convenience
+    # categoricals last: making dummies adds columns and if they're all 
+    # on the end the numerical columns don't move to new indices
+    
+    cats, nums, pows = sort_columns_by_type(X.columns)
+    X = pd.concat([X[nums], X[pows], X[cats]], axis=1)
+    X[cats] = X[cats].astype('category')
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                        test_size=0.2, 
+                                                        stratify=y, 
+                                                        random_state=3)
+    
+    return X_train, X_test, y_train, y_test
         
-    return selected_categorical, selected_numerical
+  
 
-
-
-def resample_data(X, y, strategy):
-    """resample_data(X, y, strategy):
-          X = training features for telemarketing project
-          y = training target for telemarketing project
-          strategy = one of 'ros' - RandomOverSampler
-                      'smote' - SMOTENC             
-                      ''      - No resampling 
-        returns:
-            X_resampled, y_resampled
-    """
+def run_a_model(model_name, model, columns, strategy, param_candidates):
+    
+    # -- get the data  -- #
+    X_train, X_test, y_train, y_test = read_data(columns)
+    
+    X = pd.concat([X_train, X_test], axis=0)
+    X_cols = list(X.columns)
+    
+    categorical, numerical, power = sort_columns_by_type(X_cols)
+    cat_idx = [ X_cols.index(co) for co in categorical ]
+    num_idx = [ X_cols.index(co) for co in numerical ]
+    pwr_idx = [ X_cols.index(co) for co in power ]
+    
+    transformers = []
+    if len(numerical) > 0: 
+        std = StandardScaler()
+        transformers += [ ('std', std, num_idx)]
+    if len(power) > 0:
+        pwt = PowerTransformer()
+        transformers += [ ('pwr', pwt, pwr_idx)]
+        
+    if len(categorical) > 0: 
+        # -- the unique()[1:] grabs the unique values then drops the first
+        # -- the astype(str) is because I used pandas astype categorical in the 
+        #    read_data step, this changes it back to string for sklearn
+        drop_first = {}
+        for i,co in enumerate( categorical ):
+            drop_first[i] = X[co].unique()[1:].astype(str)
+    
+        one_hot = OneHotEncoder(categories=drop_first, 
+                         handle_unknown='ignore',
+                           sparse=False)
+        transformers += [ ('oneh', one_hot, cat_idx)]
+    
+    col_xform = ColumnTransformer(transformers)
+    
+    # -- resampling needs to be added to the model with the 
+    # -- imblearn version of make_pipeline
     if strategy == "ros":
-        ros = RandomOverSampler()
-        X_resampled, y_resampled = ros.fit_resample(X, y)
+        ros = RandomOverSampler(random_state=123)
+        pipeline_model = imb_make_pipeline(ros, col_xform, model)
         
     elif strategy == "smote":
-        c,n = sort_columns_by_type(X.columns)
-        smote = SMOTENC(c, random_state=123)
-        X_resampled, y_resampled = smote.fit_resample(X, y)
+        # -- Use the NC for non-continuous data! -- #
+        smote = SMOTENC(cat_idx, random_state=123)
+        pipeline_model = imb_make_pipeline(smote, col_xform, model)
         
+    elif strategy == "down":
+        down = RandomUnderSampler(random_state=123)
+        pipeline_model = imb_make_pipeline(down, col_xform, model)
+    
     elif strategy == "":
-        X_resampled = X
-        y_resampled = y 
+        # -- Could use sklearn pipeline since there's no sampling here
+        pipeline_model = imb_make_pipeline(col_xform, model) 
         
     else:
-        raise Exception("resample_data: Unknown strategy")
-
-    return X_resampled, y_resampled
-
-
-
-def read_prepare_data(columns, strategy):
-    """read_data(columns, strategy)
-            columns = Columns to include in the model, must include 
-                    index bank_addl_id and target feature success
-            strategy = Resampling strategy ('ros', 'smote') or empty string
-                    for no resampling 
-                    
-        returns:
-            X_train, X_test, y_train, y_test
+        raise Exception("run_a_model: unknown imbalance strategy")
             
-        data read in, resampled, scaled and one-hot encoded
-    """
-    database = "postgresql://localhost/bankcalls"
+    # -- run the model -- #
+    if param_candidates == None:
+        
+        scores = cross_validate(pipeline_model, X_train, y_train, 
+                   scoring='f1', cv=4,
+                    return_estimator=True,
+                    return_train_score=False)
     
-    sql_test  = "SELECT " + ", ".join(columns) + " FROM test_group;"
-    df_test =  pd.read_sql(sql_test, database, index_col='bank_addl_id')
+        # -- Best score from cross_validate results
+        best = np.argmax(scores['test_score'])
+        best_model = scores['estimator'][best]
+        best_params = None
+        
+    else:
+        grid = GridSearchCV(pipeline_model, param_candidates,
+                            scoring='f1',
+                            cv=4, 
+                            n_jobs=-1,
+                            iid=False, 
+                            return_train_score=False)
+        grid.fit(X_train, y_train)
+        best_model = grid.best_estimator_
+        best_params = grid.best_params_
     
-    sql_train = "SELECT " + ", ".join(columns) + " FROM train_group;"
-    df_train = pd.read_sql(sql_train, database, index_col='bank_addl_id')
+    y_predict = best_model.predict(X_train)
+    
+    # -- calculate all the stats -- #
+    accuracy  = metrics.accuracy_score(y_train, y_predict)
+    recall    = metrics.recall_score(y_train, y_predict)
+    precision = metrics.precision_score(y_train, y_predict)
+    f1        = metrics.f1_score(y_train, y_predict)
+    cm        = metrics.confusion_matrix(y_train, y_predict)
+    
+    probs = pd.DataFrame(best_model.predict_proba(X_train))
+    fpr, tpr, thresholds = metrics.roc_curve(y_train, probs.loc[:,1])
+                                           
+    auc = metrics.auc(fpr, tpr)
+    
+    # -- get final column names after one hot encoding -- #
+    columns = X_cols
+    if len(categorical) > 0:
+        fake_cols = []
+        for k in range(len(drop_first.keys())):
+            for f in drop_first[k]:
+                name = categorical[k] + '_' + f
+                fake_cols += [name]
+            columns.remove(categorical[k])
+        columns += fake_cols
+                
+    # -- store the results in dictionary -- #
+    measures = dict(name=model_name, 
+                    model=best_model, 
+                    best_params=best_params,
+                    columns=columns, 
+                    strategy=strategy,
+                    accuracy=accuracy, 
+                    recall=recall, 
+                    precision=precision, 
+                    f1=f1,
+                    auc=auc, 
+                    cm=cm)                                         
+                                       
+    return measures
+    
+    
 
-    categorical, numerical = sort_columns_by_type(columns)
-    
-    for c in categorical:
-        df_test[c]  = df_test[c].astype('category')
-        df_train[c] = df_train[c].astype('category')
-        
-    y_test = df_test['success'].replace({'yes':1, 'no':0})
-    X_test = df_test.drop(columns='success')
-    
-    y_train = df_train['success'].replace({'yes':1, 'no':0})
-    X_train = df_train.drop(columns='success')
-    
-    X_train, y_train = resample_data(X_train, y_train, strategy)
-    
-    standard = StandardScaler()
-    X_train_scaled = standard.fit_transform(X_train[numerical])
-    X_test_scaled = standard.transform(X_test[numerical])
-    
-    X_train_scaled = pd.DataFrame(data=X_train_scaled, 
-                                 index=X_train.index,
-                                 columns=numerical)
-    X_test_scaled = pd.DataFrame(data=X_test_scaled,
-                                index=X_test.index,
-                                columns=numerical)
-    
-    X_train_dummies = pd.get_dummies(X_train[categorical], 
-                                     drop_first=True)
-    X_test_dummies = pd.get_dummies(X_test[categorical], 
-                                     drop_first=True)
-    
-    X_train_prepped = pd.concat([X_train_scaled, X_train_dummies],
-                               axis=1, ignore_index=False)
-    X_test_prepped = pd.concat([X_test_scaled, X_test_dummies],
-                               axis=1, ignore_index=False)
-    
-    return X_train_prepped, X_test_prepped, y_train, y_test
-        
-    
-    
-    
